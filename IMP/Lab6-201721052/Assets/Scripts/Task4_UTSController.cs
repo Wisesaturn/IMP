dbb0c04e9b16b906cf0550230046e022ffe68cba
Jae Han Song <rfv1479@gmail.com>
2022-05-26 10:05:46 +0900

    Feat #5 :: Bug fix

--- IMP/Lab6-201721052/Assets/Scripts/Task4_UTSController.cs
+++ IMP/Lab6-201721052/Assets/Scripts/Task4_UTSController.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Task4_UTSController : MonoBehaviour
 {
+    public bool Gameover = false;
+
     public void GrabUTS() {
         GameObject[] objects = GameObject.FindGameObjectsWithTag("Trash");
         foreach (GameObject obj in objects)
             Destroy(obj);
         
         Debug.Log("Level 3 Cleared!");
-        Time.timeScale = 0;
+        Gameover = true;
+        // Time.timeScale = 0;
         Application.Quit();
     }
 }
